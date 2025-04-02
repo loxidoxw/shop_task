cd9c9feeacc396e36b4463a00014edaaa4f01a31
Anton <anton.guy05@gmail.com>
2025-04-02 23:55:34 +0200

    changed Filter model

--- app/Models/Filter.php
+++ app/Models/Filter.php
@@ -3,8 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Filter extends Model
 {
-    //
+    protected $fillable = ['id', 'type', 'value'];
 }
