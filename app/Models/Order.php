a839f34b84eedefc1d9057bc8db2a076a6a3ed35
Anton <anton.guy05@gmail.com>
2025-04-04 23:59:54 +0200

    changed product model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $fillable = ['user_id', 'total_price', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,9 @@ class Product extends Model
 {
     use HasFactory;
     protected $fillable = ['id', 'name', 'description', 'price', 'category_id', 'image'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
